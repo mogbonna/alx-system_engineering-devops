772c12a8722bf02eb94da3ffe16c556c75b30f29
mogbonna <mogbonna045@gmail.com>
2024-01-20 09:10:38 -0800

    correction to task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # Install a version of flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Package['python3-pip']
+}
+
+package { 'python3-pip':
+  ensure   => present,
   provider => 'pip3'
 }
+
